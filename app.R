9714e4e63ce15c5f41d43b6ef954f4c2df799682
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2023-04-13 19:20:42 +0900

    Update app.R

--- app.R
+++ app.R
@@ -2667,7 +2667,7 @@ server <- function(input, output, session) {
             fluidRow(style = "position:relative",
                      tabBox(width = 12, title = strong("Forest Plot", style = "color:black"),
                             tabPanel("Divide-Aggregate Composite-Null Test", align = "center",
-                                     plotOutput("DACT_forestplot", height = 600, width = 750))))
+                                     plotOutput("DACT_forestplot", height = 500, width = 700))))
           })
           
           output$DACT_forestplot <- renderPlot({
