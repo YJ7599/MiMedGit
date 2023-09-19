4871cc473690701395772654f1355d922fb9b557
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2023-09-19 14:07:50 +0900

    Update app.R

--- app.R
+++ app.R
@@ -3128,7 +3128,7 @@ server <- function(input, output, session) {
           
           if (binary_cont(chooseData$sam.dat[,input$treat_taxa]) == "binary"){
           
-            med_result <<- tryCatch(mediation.taxon.total(chooseData$sam.dat, taxa, taxa_unique, input$treat_taxa, covariate_taxa, input$outcome_taxa, interaction_taxa, taxa_reg_type, taxa_sim_type, boot.method.po = boot.iterations, n.sim = 10, inc = include), 
+            med_result <<- tryCatch(mediation.taxon.total(chooseData$sam.dat, taxa, taxa_unique, input$treat_taxa, covariate_taxa, input$outcome_taxa, interaction_taxa, taxa_reg_type, taxa_sim_type, boot.method.po = boot.iterations, n.sim = 2000, inc = include), 
                                     error = function(e) {
                                       message("No outcome is available!")
                                       showModal(modalDialog(div("No outcome is available!")))
@@ -3732,7 +3732,7 @@ server <- function(input, output, session) {
             
             chooseData$sam.dat[[input$outcome_taxa]] <- as.numeric(chooseData$sam.dat[[input$outcome_taxa]])
             
-            med_result <<- tryCatch(mediation.taxon.total(chooseData$sam.dat, taxa, taxa_unique, input$treat_taxa, covariate_taxa, input$outcome_taxa, interaction_taxa, taxa_reg_type, taxa_sim_type, boot.method.po = boot.iterations, n.sim = 10, inc = include), 
+            med_result <<- tryCatch(mediation.taxon.total(chooseData$sam.dat, taxa, taxa_unique, input$treat_taxa, covariate_taxa, input$outcome_taxa, interaction_taxa, taxa_reg_type, taxa_sim_type, boot.method.po = boot.iterations, n.sim = 2000, inc = include), 
                                    error = function(e) {  
                                      message("No outcome is available!")
                                      showModal(modalDialog(div("No outcome is available!")))
